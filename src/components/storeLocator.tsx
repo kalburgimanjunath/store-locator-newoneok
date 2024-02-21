8853af0cffd22e1a8bae3c085e99758e84e31157
Manjunath Kalburgi <manjunathkalburgi@gmail.com>
2024-02-21 10:28:47 +0000

    map added

--- src/components/storeLocator.tsx
+++ src/components/storeLocator.tsx
@@ -3,6 +3,7 @@ import FilterDistributor from "./filterDistributor";
 import { useState } from "react";
 import MapDistributor from "./mapDistributor";
 export default function StoreLocator() {
+  const [selectedDistributor, setDistributor] = useState();
   const [location, setLocation] = useState("");
   const filterLocations = DISTRIBUTORS.filter((item) => {
     return item.address.toLowerCase().includes(location.toLowerCase());
@@ -12,7 +13,17 @@ export default function StoreLocator() {
       <h1>Store Locator</h1>
       <FilterDistributor setLocation={setLocation} />
       <div className="grid grid-cols-2 border-2 shadow-lg">
-        <MapDistributor />
+        {selectedDistributor ? (
+          <div>
+            <div onClick={() => setDistributor(null)}>Back to Maps</div>
+            <div>{selectedDistributor.name}</div>
+            <div>{selectedDistributor.address}</div>
+            <div>{selectedDistributor.phone}</div>
+          </div>
+        ) : (
+          <MapDistributor />
+        )}
+
         <div className="p-4 text-left">
           <h3 className="font-bold text-2xl border-b-2 p-2">Distributors</h3>
           {filterLocations &&
@@ -20,6 +31,7 @@ export default function StoreLocator() {
               return (
                 <div
                   key={item.name}
+                  onClick={() => setDistributor(item)}
                   className="p-2 cursor-pointer hover:bg-pink-300"
                 >
                   {item.name}
